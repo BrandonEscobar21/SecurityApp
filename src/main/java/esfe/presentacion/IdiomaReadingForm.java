8e98689a77f1d154668622f563783a996f888901
BrandonEscobar21 <brandonparedesacb2020@gmail.com>
2025-06-13 17:18:39 -0600

    Diseño de los formularios de Idioma

---
+++ src/main/java/esfe/presentacion/IdiomaReadingForm.java
@@ -0,0 +1,12 @@
+package esfe.presentacion;
+
+import javax.swing.*;
+
+public class IdiomaReadingForm {
+    private JPanel mainPanel;
+    private JTextField txtName;
+    private JButton btnCreate;
+    private JTable tableIdioma;
+    private JButton btnUpdate;
+    private JButton btnDelete;
+}
---
+++ src/main/java/esfe/presentacion/IdiomaWriteForm.java
@@ -0,0 +1,12 @@
+package esfe.presentacion;
+
+import javax.swing.*;
+
+public class IdiomaWriteForm {
+    private JPanel mainPanel;
+    private JTextField txtName;
+    private JTextField txtFamily;
+    private JTextField txtNumber;
+    private JButton btnOk;
+    private JButton btnCancel;
+}
